8338898f16af4898d7eff60ad7015f9df84dfbe6
hznagui <houdayfahouayfa7@gmail.com>
2024-03-21 22:11:51 +0000

    done deal

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>php site</title>
+        <link rel="icon" href="">
+    </head>
+    <body>
+        <?php
+        echo("<h1>hello hello</h1>\n");
+        echo "<hr>";
+        echo "<p>waw zahya<p>";
+
+        ?>
+    </body>
+</html>
