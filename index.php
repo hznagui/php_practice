59a1f676da5532c931ec2a1f82c4dfed3e615923
hznagui <houdayfahouayfa7@gmail.com>
2024-03-23 15:49:20 +0000

    adding numbers.php and starting a arrays

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
+
+<?php 
+    $age=50;//this is how can we declare a variable in php
+    $firstname='tamago';//this is how can we declare a string in php
+    $lastname="znagui";//another way
+    define('NAME',"houdaifa znagui")//this way we can creat a const 
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
-<html>
+
     <head>
         <meta charset="UTF-8">
         <title>php site</title>
         <link rel="icon" href="">
+        <meta name="author" content=<?php echo $firstname.$lastname;?>>
     </head>
     <body>
         <?php
         echo("<h1>hello hello</h1>\n");
-        echo "<hr>";
-        echo "<p>waw zahya<p>";
-
+        echo NAME;
+        echo "\n $firstname $lastname\n";
         ?>
+        <hr>
+        <?php
+        echo "<p>waw zahya<p>";
+            ?>
+        <a href="numbers.php"> to numbers page </a>
+        
     </body>
 </html>
---
+++ numbers.php
@@ -0,0 +1,13 @@
+
+<?php
+echo "<h1>numbers</h1>";
+//operation in php + - / * pow is **
+//every thing else is like cpp
+    $count1 = 10.6;
+
+    $count2 = 80;
+    $count1++;
+    --$count1;
+    echo round($count1);
+    echo '\n<a href="/arrays.php"> for arrays</a>'
+?>
