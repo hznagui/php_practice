c3e69333755b445add816164394e4b36cca258cb
hznagui <houdayfahouayfa7@gmail.com>
2024-03-23 15:49:27 +0000

    adding numbers.php and starting a arrays

--- numbers.php
+++ numbers.php
@@ -3,6 +3,8 @@
 echo "<h1>numbers</h1>";
 //operation in php + - / * pow is **
 //every thing else is like cpp
+//BIDMAS b:bracket i:indices d:division m:multiplication a:addition s:substraction
+//BIDMAS is the concept that math work with
     $count1 = 10.6;
 
     $count2 = 80;
