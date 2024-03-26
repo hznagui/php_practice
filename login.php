65a97403ff5fb5ba39ea2a6c275ef7fa2baacc37
hznagui <houdayfahouayfa7@gmail.com>
2024-03-26 14:07:30 +0000

    updating_file

--- login.php
+++ login.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
 <?php  
+session_start();
 require_once('./inc/config.php');
 require_once('./inc/library.php');
-session_start();
 if(isAthenticated())
-    {
-        redirect('admin');
-        die();
-    }
+{
+    redirect('admin');
+    die();
+}
 $email = $_POST['email'] ?? ""; 
 $bool = "";
 $password=$_POST['password'] ?? "";
@@ -18,10 +17,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && author($email, $password))
     die();
 }
 else if ($_SERVER['REQUEST_METHOD'] == 'POST')
-    $bool = "the provided credentials didn't not work!";
+$bool = "the provided credentials didn't not work!";
 include ('./inc/header.php');
 // echo "<br> $email - $password";
 ?>
+<!DOCTYPE html>
 
 <div class="container">
     <div class="row">
@@ -32,13 +32,14 @@ include ('./inc/header.php');
             <form action="" method="POST">
                 <div class="form-group">
                     <label for="email">email</label>
-                    <input name="email" id="email" type="text" class="form-control">
+                    <input name="email" id="email" type="text" class="form-control" placeholder="emai...">
                 </div>
                 <div class="form-group">
                     <label for="password">password</label>
-                    <input name="password" id="password" type="password" class="form-control">
+                    <input name="password" id="password" type="password" class="form-control" placeholder="password">
                 </div>
-                <input type="submit" value="login" class="btn  btn-outline-danger btn-lg btn-block">
+                <br>
+                <input type="submit" value="login" class="w-100 btn btn-lg btn-outline-danger">
             </form>
         </div>
     </div>
